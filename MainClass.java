61ff9cce54c249122b43512c24f6433ff1bfd68b
vijaykiran225 <vijaykiran225@gmail.com>
2015-07-29 12:39:56 +0530

    Updated PGN file and MainClass can now process the file

--- MainClass.java
+++ MainClass.java
@@ -10,11 +10,16 @@ public class MainClass {
 	
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
-		BufferedReader reader=new BufferedReader(new FileReader("sample.pgn"));
+		BufferedReader reader=new BufferedReader(new FileReader("./src/PGN/sample.pgn"));
 		String fileContent="";
+		ChessGame game=new ChessGame();
 		while((fileContent=reader.readLine())!=null)
 		{
-			System.out.println(fileContent);
+			//System.out.println(fileContent);
+			String[] line=fileContent.split("\\.");
+			String[] move=line[1].split(" ");
+			System.out.println(move[0]+"-"+move[1]);
+			//game.makeMove(move[0], move[1]);
 		}
 	}
 
