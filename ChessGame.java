fb33914cdfb3cebb6ca09613aa10d31e8e770a29
Sri Harish <test@L-MAA-25004247.paypalcorp.com>
2015-07-29 17:31:12 +0530

    Modified ChessGame so that it uses isNotCaptured()

--- ChessGame.java
+++ ChessGame.java
@@ -157,7 +157,7 @@ public class ChessGame {
 
 		int count = 0;
 		for (ChessPiece chessPiece : chessPieces) {
-			if (chessPiece.getName().equals(name) && chessPiece.getColor().equals(color) && !chessPiece.isCaptured()) {
+			if (chessPiece.getName().equals(name) && chessPiece.getColor().equals(color) && !chessPiece.isNotCaptured()) {
 				count++;
 			}
 		}
@@ -165,7 +165,7 @@ public class ChessGame {
 		int k = 0;
 		for (int i = 0; i < chessPieces.size(); i++) {
 
-			if (chessPieces.get(i).getName().equals(name) && chessPieces.get(i).getColor().equals(color) && !chessPieces.get(i).isCaptured()) {
+			if (chessPieces.get(i).getName().equals(name) && chessPieces.get(i).getColor().equals(color) && !chessPieces.get(i).isNotCaptured()) {
 				indexes[k] = i;
 				k++;
 			}
